4d0232d98b0a8650b3b29be5ea18b7abfc726334
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2016-12-16 12:21:35 -0800

    added rudimentory error checking, will currently always return error due to not handling 20th byte properly

--- ft_get_tetro.c
+++ ft_get_tetro.c
@@ -6,11 +6,35 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 11:52:22 by vyudushk          #+#    #+#             */
-/*   Updated: 2016/12/11 15:14:13 by vyudushk         ###   ########.fr       */
+/*   Updated: 2016/12/16 12:20:53 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfil.h"
+
+/*
+** Will return 0 if tetromino is valid.
+*/
+
+int		ft_check_valid(char tet)
+{
+	int	n;
+
+	if (tet[4] != '\n' || tet[9] != '\n' || tet[14] != '\n' || tet[19] != '\n')
+		return (1);
+	if (tet[20] != '\n')
+		return (2);
+	if (tet[20] != 0)
+		return (3);
+	n = 0;
+	while (n != 19)
+	{
+		if (tet[n] != '.' || tet[n] != '#' || tet[n] != '\n')
+			return (4);
+		n++;
+	}
+	return (0);
+}
 
 char	*ft_get_tetro(char *arg)
 {
