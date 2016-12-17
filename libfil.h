ce50135d1ee0e3f50caa6664665b7efe21c677ea
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2016-12-16 18:34:11 -0800

    added block struct

--- libfil.h
+++ libfil.h
@@ -6,11 +6,11 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 14:37:50 by vyudushk          #+#    #+#             */
-/*   Updated: 2016/12/11 15:12:18 by dnelson          ###   ########.fr       */
+/*   Updated: 2016/12/16 17:25:42 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	LIBFIL_H
+#ifndef LIBFIL_H
 # define LIBFIL_H
 # include <unistd.h>
 # include <stdlib.h>
@@ -18,6 +18,13 @@
 # include <string.h>
 # include "libft.h"
 
-char	*ft_get_tetro(char *arg);
+char			*ft_get_tetro(char *arg);
+
+typedef struct	s_block
+{
+	int	x[5];
+	int	y[5];
+	struct s_block	*next;
+}				t_block;
 
 #endif
