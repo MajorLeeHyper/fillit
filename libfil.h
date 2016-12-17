bdb9dae7f474c045c79e1b3f6640b94bb1788479
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2016-12-16 19:18:47 -0800

    view coords as 2d array instead of two seperate int arrays

--- libfil.h
+++ libfil.h
@@ -6,7 +6,7 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 14:37:50 by vyudushk          #+#    #+#             */
-/*   Updated: 2016/12/16 17:25:42 by vyudushk         ###   ########.fr       */
+/*   Updated: 2016/12/16 19:18:12 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ char			*ft_get_tetro(char *arg);
 
 typedef struct	s_block
 {
-	int	x[5];
-	int	y[5];
+	int	coord[4][4];
 	struct s_block	*next;
 }				t_block;
 
