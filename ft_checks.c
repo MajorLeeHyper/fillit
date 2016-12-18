45b98a47b207de175cc196fb512633695b5da79b
Daniel Nelson <dan.nelson001@gmail.com>
2016-12-17 20:24:27 -0800

    making it look better

--- ft_checks.c
+++ ft_checks.c
@@ -6,7 +6,7 @@
 /*   By: dnelson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 13:42:07 by dnelson           #+#    #+#             */
-/*   Updated: 2016/12/17 20:22:47 by vyudushk         ###   ########.fr       */
+/*   Updated: 2016/12/17 20:24:10 by dnelson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,22 @@ int		ft_connection(char **tet)
 		{
 			if (tet[i][j] == '#')
 			{
-				if (i != 0 && tet[i - 1][j] == '#')
+				if (i > 0 && tet[i - 1][j] == '#')
 				{
 					count++;
 					ft_putstr("column count++\n");
 				}
-				if (i != 3 && tet[i + 1][j] == '#')
+				if (i < 3 && tet[i + 1][j] == '#')
 				{
 					ft_putstr("column count++\n");
 					count++;
 				}
-				if (j != 0 && tet[i][j - 1] == '#')
+				if (j > 0 && tet[i][j - 1] == '#')
 				{
 					count++;
 					ft_putstr("row count++\n");
 				}
-				if (j != 3 && tet[i][j + 1] == '#')
+				if (j < 3 && tet[i][j + 1] == '#')
 				{
 					ft_putstr("row count++\n");
 					count++;
@@ -52,7 +52,7 @@ int		ft_connection(char **tet)
 		}
 		i++;
 	}
-	if (count >= 6)
+	if (count == 6 || count == 8)
 		return (1);
 	return (0);
 }
